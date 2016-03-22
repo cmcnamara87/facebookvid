9c9c241e6e9fa7e67d7ba421a14619939f12f235
Craig McNamara <cmcnamara87@gmail.com>
2016-03-22 16:51:43 +1000

    feat: add description of use

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,44 +2,43 @@
 <html>
     <head>
         <title>Laravel</title>
-
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
     </head>
     <body>
         <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-            </div>
+            <h2>Request</h2>
+            Post to
+            <pre>
+                http://128.199.104.251/facebookvid/current/public/video
+            </pre>
+            Body
+            <pre>
+                application/json
+            </pre>
+            <pre>
+                {
+                    slides: [... array of slides ...]
+                }
+            </pre>
+
+            <h3>Slides</h3>
+            <h4>Title</h4>
+            <pre>
+                [
+                    "title" => "INDIAN FOOD",
+                    "background_color" => "#9B3166",
+                    "text_color" => "#fff"
+                ]
+            </pre>
+            <h4>Subtitle with background image</h4>
+            <pre>
+                [
+                   "title" => "Cooking Demo",
+                    "subtitle" => "Booval",
+                    "background_color" => "#0F1219",
+                    "image_url" => "http://static.au.groupon-content.net/70/69/1454489676970.jpg",
+                    "text_color" => "#fff"
+                ]
+            </pre>
         </div>
     </body>
 </html>
